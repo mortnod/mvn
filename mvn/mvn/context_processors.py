9c5cbcb4accc234906c9d5a7f3e22320bee4aa1d
Morten Vaale Noddeland <morten@noddeland.no>
2012-06-06 20:26:06 +0200

    Bug fix: No quotes in database does no longer make the project crash

--- mvn/mvn/context_processors.py
+++ mvn/mvn/context_processors.py
@@ -2,6 +2,10 @@ from quotes.models import Quote
 import random
 
 def random_quote(request):
-	total = Quote.objects.count()
-	id = random.randint(1, total)
-	return {'quote': Quote.objects.get(pk=id)}
+    total = Quote.objects.count()
+    if total == 0:
+        quote = Quote(quote="These are not the quotes you're looking for", name="The system administrator") 
+        return {'quote': quote}
+    else:
+        id = random.randint(1, total)
+        return {'quote': Quote.objects.get(pk=id)}
